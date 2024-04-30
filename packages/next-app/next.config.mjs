1bb38d7b933ff19d43a3c0d785a3b2c52672d1f5
Oleg Dashevskii <olegdashevskii@gmail.com>
2024-04-30 08:55:10 +0700

    next-app

--- packages/next-app/next.config.mjs
+++ packages/next-app/next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@nodemono2/lib2', 'ssh2'],
   },
-  output: 'standalone',
+  ...(process.env.BAZEL_BINDIR == null ? {
+    // standalone output doesn't work for Bazel
+    output: 'standalone',
+  } : null),
 };
 
 export default nextConfig;
