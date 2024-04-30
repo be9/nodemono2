6288691f07de0e136f31d6e6c0ffcbda11706940
Oleg Dashevskii <olegdashevskii@gmail.com>
2024-04-30 13:44:58 +0700

    server/index

--- packages/next-app/server/index.js
+++ packages/next-app/server/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const {createServer} = require('http');
 const {parse} = require('url');
 const next = require('next');
@@ -8,6 +9,8 @@ const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({dev, hostname, port});
 const handle = app.getRequestHandler();
+
+console.log("RUNNING IN", process.cwd());
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
